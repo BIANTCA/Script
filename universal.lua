2b34ad947aad53ba26b41bcd785cec6ad3e8414f
BIANTCA <nurfadhilla120604@gmail.com>
2026-01-05 23:24:30 +0700

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -665,6 +665,24 @@ MainTab:CreateToggle({
 })
 
 -- ======== TOOLS TAB ========
+local remoteTabInstance = nil
+
+ToolsTab:CreateToggle({
+	Name = "Remote Tools",
+	CurrentValue = false,
+	Callback = function(v)
+		if v then
+			local RemoteLogger = loadstring(game:HttpGet("https://pastebin.com/raw/3C5BF1cc"))()
+			remoteTabInstance = RemoteLogger.CreateRemoteLoggerTab(Window, Rayfield, Players, RunService)
+		else
+			if remoteTabInstance and remoteTabInstance.Tab and remoteTabInstance.Tab.Destroy then
+				remoteTabInstance.Tab:Destroy()
+				remoteTabInstance = nil
+			end
+		end
+	end
+})
+
 local antiAFKConn, antiHooked, oldNamecall
 
 ToolsTab:CreateSection("Tools utility")
